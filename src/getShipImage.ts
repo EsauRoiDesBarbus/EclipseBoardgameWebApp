65cfece28d0aa177f14248c845e6e44755ea1d85
Hugo Voisin <hugov@bam.tech>
2024-05-10 17:57:19 +0200

    feat: add npc icons

--- src/getShipImage.ts
+++ src/getShipImage.ts
@@ -2,6 +2,9 @@ import InterceptorImage from './assets/ship/interceptor.png'
 import CruiserImage from './assets/ship/cruiser.png'
 import DreadnoughtImage from './assets/ship/dreadnought.png'
 import StarbaseImage from './assets/ship/starbase.png'
+import AncientImage from './assets/ship/ancient.png'
+import GardianImage from './assets/ship/guardian.png'
+import GcdsImage from './assets/ship/gcds.png'
 
 import type { ShipType } from './types'
 
@@ -15,6 +18,13 @@ export const getShipImage = (shipType: ShipType | 'ancient' | 'gardian' | 'gcds'
       return DreadnoughtImage
     case 'starbase':
       return StarbaseImage
+    case 'ancient':
+    case 'npc':
+      return AncientImage
+    case 'gardian':
+      return GardianImage
+    case 'gcds':
+      return GcdsImage
     default:
       return InterceptorImage
   }
