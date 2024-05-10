2e29eef17e7b35f6abecd91957a4f260592a3a24
Hugo Voisin <hugov@bam.tech>
2024-05-10 15:32:05 +0200

    feat: can add ship blueprints (default is zero ships)

--- src/App.tsx
+++ src/App.tsx
@@ -47,20 +47,21 @@ function App() {
 
   const { register, control, handleSubmit, watch } = useForm<FormValues>({
     defaultValues: {
-      attackerShips: Array(2).fill(defaultShip),
-      defenderShips: Array(2).fill(defaultShip),
+      attackerShips: [],
+      defenderShips: [],
     },
   })
 
-  const { fields: attackerShipFields } = useFieldArray({
+  const { fields: attackerShipFields, append: attackerShipAppend } = useFieldArray({
     control,
     name: 'attackerShips',
   })
-  const { fields: defenderShipFields } = useFieldArray({
+  const { fields: defenderShipFields, append: defenderShipAppend } = useFieldArray({
     control,
     name: 'defenderShips',
   })
   const fields = { attackerShips: attackerShipFields, defenderShips: defenderShipFields }
+  const appendFunctions = { attackerShips: attackerShipAppend, defenderShips: defenderShipAppend }
 
   const onSubmit = async (data: FormValues) => {
     const result = await getSimulationResult(data)
@@ -69,165 +70,203 @@ function App() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column' }}>
-      {(['attackerShips', 'defenderShips'] as const).map((shipType) => (
-        <div key={shipType}>
-          <h2>{shipType === 'attackerShips' ? 'Attack' : 'Defense'}</h2>
-          {fields[shipType].map((field, index) => {
-            const shipTypeValue = watch(`${shipType}.${index}.type`)
-            return (
-              <div
-                key={field.id}
-                style={{
-                  marginBottom: '20px',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: 16,
-                  padding: 16,
-                  borderRadius: 8,
-                }}
-                className="elevation-1">
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <div
-                    style={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      gap: 8,
-                      justifyContent: 'space-between',
-                    }}>
-                    <label htmlFor={`ships.${index}.type`}>Type</label>
-                    <select
-                      {...register(`${shipType}.${index}.type`)}
-                      id={`${shipType}.${index}.type`}
-                      style={{ display: 'flex', flexGrow: 1, maxHeight: 36 }}>
-                      <option value="interceptor">Interceptor</option>
-                      <option value="cruiser">Cruiser</option>
-                      <option value="dreadnought">Dreadnought</option>
-                      <option value="starbase">Star base</option>
-                    </select>
-                  </div>
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.count`}
-                    min={1}
-                    label="Number"
-                    accessibilityLabel="increase number of ships"
-                    image={getShipImage(shipTypeValue)}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.initiative`}
-                    label="Initiative"
-                    accessibilityLabel="increase initiative"
-                    image={InitiativeImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.hull`}
-                    label="Hull"
-                    accessibilityLabel="increase hull"
-                    image={HullImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.computer`}
-                    label="Computer"
-                    accessibilityLabel="increase computer"
-                    image={ComputerImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.shield`}
-                    label="Shield"
-                    accessibilityLabel="increase shield"
-                    image={ShieldImage}
-                  />
-                  {shipType === 'defenderShips' ? (
-                    <div style={{ display: 'flex', alignItems: 'center', gap: 4 }}>
-                      <input
-                        type="checkbox"
-                        id={`${shipType}.${index}.isNpc`}
-                        {...register(`${shipType}.${index}.isNpc`)}
+      {(['attackerShips', 'defenderShips'] as const).map((shipSide) => (
+        <div key={shipSide}>
+          <h2>{shipSide === 'attackerShips' ? 'Attack' : 'Defense'}</h2>
+          <div style={{ display: 'flex', flexDirection: 'column', gap: 20 }}>
+            <div
+              className="elevation-1"
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                padding: '0px 16px 16px 16px',
+              }}>
+              <h3>
+                Add blueprint on {shipSide === 'attackerShips' ? 'attacker' : 'defender'} side
+              </h3>
+              <div style={{ display: 'flex', gap: 16 }}>
+                {(['interceptor', 'cruiser', 'dreadnought', 'starbase'] as const).map(
+                  (shipType) => (
+                    <button
+                      key={`add-${shipSide}-${shipType}`}
+                      type="button"
+                      onClick={() => {
+                        appendFunctions[shipSide]({ ...defaultShip, type: shipType })
+                      }}
+                      style={{
+                        display: 'flex',
+                        flexShrink: 0,
+                        alignItems: 'center',
+                        gap: 8,
+                        maxWidth: 180,
+                      }}>
+                      Add {shipType}
+                      <img
+                        src={getShipImage(shipType)}
+                        height={30}
+                        width={30}
+                        alt={`add ${shipType}`}
                       />
-                      <label htmlFor={`${shipType}.${index}.isNpc`}>NPC</label>
-                    </div>
-                  ) : null}
-                </div>
-                <div
-                  style={{
-                    display: 'grid',
-                    gridTemplateColumns: 'repeat(6, 1fr)',
-                    padding: 8,
-                    gap: 16,
-                    borderRadius: 8,
-                  }}
-                  className="elevation-2">
-                  <label style={{ display: 'flex', alignSelf: 'center', justifySelf: 'center' }}>
-                    Cannons
-                  </label>
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.cannon.yellow`}
-                    accessibilityLabel="increase yellow cannon"
-                    image={YellowWeaponImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.cannon.orange`}
-                    accessibilityLabel="increase orange cannon"
-                    image={OrangeWeaponImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.cannon.blue`}
-                    accessibilityLabel="increase blue cannon"
-                    image={BlueWeaponImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.cannon.red`}
-                    accessibilityLabel="increase red cannon"
-                    image={RedWeaponImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.cannon.pink`}
-                    accessibilityLabel="increase rift cannon"
-                    image={PinkWeaponImage}
-                  />
-                  <label style={{ display: 'flex', alignSelf: 'center', justifySelf: 'center' }}>
-                    Missiles
-                  </label>
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.missile.yellow`}
-                    accessibilityLabel="increase yellow missile"
-                    image={YellowWeaponImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.missile.orange`}
-                    accessibilityLabel="increase orange missile"
-                    image={OrangeWeaponImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.missile.blue`}
-                    accessibilityLabel="increase blue missile"
-                    image={BlueWeaponImage}
-                  />
-                  <NumberInput
-                    control={control}
-                    name={`${shipType}.${index}.missile.red`}
-                    accessibilityLabel="increase red missile"
-                    image={RedWeaponImage}
-                  />
-                </div>
+                    </button>
+                  )
+                )}
               </div>
-            )
-          })}
+            </div>
+            {fields[shipSide].map((field, index) => {
+              const shipTypeValue = watch(`${shipSide}.${index}.type`)
+              return (
+                <div
+                  key={field.id}
+                  className="elevation-1"
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 16,
+                    padding: 16,
+                  }}>
+                  <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 8,
+                        justifyContent: 'space-between',
+                      }}>
+                      <label htmlFor={`ships.${index}.type`}>Type</label>
+                      <select
+                        {...register(`${shipSide}.${index}.type`)}
+                        id={`${shipSide}.${index}.type`}
+                        style={{ display: 'flex', flexGrow: 1, maxHeight: 36 }}>
+                        <option value="interceptor">Interceptor</option>
+                        <option value="cruiser">Cruiser</option>
+                        <option value="dreadnought">Dreadnought</option>
+                        <option value="starbase">Star base</option>
+                      </select>
+                    </div>
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.count`}
+                      min={1}
+                      label="Number"
+                      accessibilityLabel="increase number of ships"
+                      image={getShipImage(shipTypeValue)}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.initiative`}
+                      label="Initiative"
+                      accessibilityLabel="increase initiative"
+                      image={InitiativeImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.hull`}
+                      label="Hull"
+                      accessibilityLabel="increase hull"
+                      image={HullImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.computer`}
+                      label="Computer"
+                      accessibilityLabel="increase computer"
+                      image={ComputerImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.shield`}
+                      label="Shield"
+                      accessibilityLabel="increase shield"
+                      image={ShieldImage}
+                    />
+                    {shipSide === 'defenderShips' ? (
+                      <div style={{ display: 'flex', alignItems: 'center', gap: 4 }}>
+                        <input
+                          type="checkbox"
+                          id={`${shipSide}.${index}.isNpc`}
+                          {...register(`${shipSide}.${index}.isNpc`)}
+                        />
+                        <label htmlFor={`${shipSide}.${index}.isNpc`}>NPC</label>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div
+                    className="elevation-2"
+                    style={{
+                      display: 'grid',
+                      gridTemplateColumns: 'repeat(6, 1fr)',
+                      padding: 8,
+                      gap: 16,
+                    }}>
+                    <label style={{ display: 'flex', alignSelf: 'center', justifySelf: 'center' }}>
+                      Cannons
+                    </label>
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.cannon.yellow`}
+                      accessibilityLabel="increase yellow cannon"
+                      image={YellowWeaponImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.cannon.orange`}
+                      accessibilityLabel="increase orange cannon"
+                      image={OrangeWeaponImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.cannon.blue`}
+                      accessibilityLabel="increase blue cannon"
+                      image={BlueWeaponImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.cannon.red`}
+                      accessibilityLabel="increase red cannon"
+                      image={RedWeaponImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.cannon.pink`}
+                      accessibilityLabel="increase rift cannon"
+                      image={PinkWeaponImage}
+                    />
+                    <label style={{ display: 'flex', alignSelf: 'center', justifySelf: 'center' }}>
+                      Missiles
+                    </label>
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.missile.yellow`}
+                      accessibilityLabel="increase yellow missile"
+                      image={YellowWeaponImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.missile.orange`}
+                      accessibilityLabel="increase orange missile"
+                      image={OrangeWeaponImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.missile.blue`}
+                      accessibilityLabel="increase blue missile"
+                      image={BlueWeaponImage}
+                    />
+                    <NumberInput
+                      control={control}
+                      name={`${shipSide}.${index}.missile.red`}
+                      accessibilityLabel="increase red missile"
+                      image={RedWeaponImage}
+                    />
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
       ))}
 
+      <div style={{ height: 20 }} />
       <button type="submit">Submit</button>
 
       {simulationResult && (
