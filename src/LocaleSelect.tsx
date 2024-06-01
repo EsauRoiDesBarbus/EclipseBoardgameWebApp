7cff9397dce04d979dfc6cb73ff0a0fa67a19f20
Hugo Voisin <hugov@bam.tech>
2024-06-01 02:28:08 +0200

    fix: fix vite build warning

--- src/LocaleSelect.tsx
+++ src/LocaleSelect.tsx
@@ -4,7 +4,7 @@ export const LocaleSelect = () => {
   const { i18n } = useLingui()
 
   const dynamicActivate = async (locale: string) => {
-    const { messages } = (await import(`./locales/${locale}`)) as {
+    const { messages } = (await import(`./locales/${locale}.ts`)) as {
       messages: Record<string, string>
     }
 
--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const defaultLocale = navigator.language.split('-')[0]
 const I18nApp = () => {
   useEffect(() => {
     const dynamicActivate = async (locale: string) => {
-      const { messages } = (await import(`./locales/${locale}`)) as {
+      const { messages } = (await import(`./locales/${locale}.ts`)) as {
         messages: Record<string, string>
       }
 
