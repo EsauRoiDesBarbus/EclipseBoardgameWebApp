30e02752222dfe7f1a5f781c499e53f8372649ed
Hugo Voisin <hugo.voisin@theodo.com>
2024-11-30 22:24:45 +0100

    types: fix new issues

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,10 @@ import { I18nProvider } from 'src/features/i18n/I18nProvider.tsx'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Root element not found')
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <I18nProvider>
       <App />
