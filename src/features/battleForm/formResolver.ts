138cefd32fcc2a1f15a02c48d54745c6b1e03b14
Hugo Voisin <hugo.voisin@theodo.com>
2024-12-01 01:32:53 +0100

    feat: improve error display with more precision in missing info

--- src/features/battleForm/formResolver.ts
+++ src/features/battleForm/formResolver.ts
@@ -2,16 +2,37 @@ import { t } from '@lingui/macro'
 
 import { FormValues } from './types'
 
+const missingAttackerShips = t`Missing attacker ships.`
+const missingDefenderShips = t`Missing defender ships.`
+
 export const formResolver = (data: FormValues) => {
-  if (data.attackerShips.length === 0 || data.defenderShips.length === 0) {
+  if (data.attackerShips.length === 0 && data.defenderShips.length === 0) {
     return {
       values: data,
       errors: {
         attackerShips: {
-          message: t`Missing attacker ships.`,
+          message: missingAttackerShips,
         },
         defenderShips: {
-          message: t`Missing defender ships.`,
+          message: missingDefenderShips,
+        },
+      },
+    }
+  } else if (data.attackerShips.length === 0) {
+    return {
+      values: data,
+      errors: {
+        attackerShips: {
+          message: missingAttackerShips,
+        },
+      },
+    }
+  } else if (data.defenderShips.length === 0) {
+    return {
+      values: data,
+      errors: {
+        defenderShips: {
+          message: missingDefenderShips,
         },
       },
     }
